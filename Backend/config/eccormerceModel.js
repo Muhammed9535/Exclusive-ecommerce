c485ecf832a6064b8cc2e7857ac7dd68813a3861
unknown <lawalolarewaju123@gmail.con>
2025-03-15 04:44:49 +0100

    final commit

--- Backend/config/eccormerceModel.js
+++ Backend/config/eccormerceModel.js
@@ -2,12 +2,17 @@ import pg from "pg";
 import "dotenv/config"
 
 
-const pool = new pg.Client({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_KEY,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+const { Pool } = pg;
+
+// const user = process.env.DATABASE_USER;
+// const database = process.env.DATABASE_KEY;
+// const password = process.env.DATABASE_PASSWORD;
+
+const connectionString = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:${process.env.DATABASE_PORT}/${process.env.DATABASE_KEY}`
+
+const pool = new Pool({
+    connectionString: connectionString, // Use an environment variable for security
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
