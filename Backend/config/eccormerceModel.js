93ea81404dd159f8ad8dc1622028365b78550555
unknown <lawalolarewaju123@gmail.con>
2025-03-15 04:47:16 +0100

    final commit

--- Backend/config/eccormerceModel.js
+++ Backend/config/eccormerceModel.js
@@ -10,6 +10,12 @@ const { Pool } = pg;
 
 const connectionString = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:${process.env.DATABASE_PORT}/${process.env.DATABASE_KEY}`
 
+
+
+
+NODE_ENV = production
+connectionString = 'postgresql://exclusive_ecormmerce_db_user:5t8a18rl5QtnsVxq0cX8fMstESnIojW5@dpg-cvabdnjqf0us73ctd65g-a.oregon-postgres.render.com/exclusive_ecormmerce_db'
+
 const pool = new Pool({
     connectionString: connectionString, // Use an environment variable for security
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
