fa50bba262a2d5ba8a112d8d74d2dc5beb633d87
Muhammed9535 <105399837+Muhammed9535@users.noreply.github.com>
2025-03-18 12:04:20 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,7 @@ app.use(session({
     cookie: {
         secure: true,
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24,
     }
 }))
